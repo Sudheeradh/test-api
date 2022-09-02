032cf8a593158ca3075dd07795b5d2b6230ee967
sudheeradh <k.sudheeradh@gmail.com>
2022-09-02 17:55:22 +0530

    api test v1

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+
+const PORT = 8000
+
+const rappers = {
+    "21 savage": {
+        'age': 29,
+        'birthName': "Abraham",
+        'location': "London"
+    },
+    "chance the rapper": {
+        "age": 29,
+        'birthName': "Chancelor",
+        'location': "Chicago"
+    },
+    "dylan": {
+        "age": 29,
+        'birthName': "Dylan",
+        'location': "Dylan"
+    }
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+// app.get('/js/main.js', (req, res) => {
+//     res.sendFile(__dirname + req.originalUrl)
+// })
+
+app.get('/api/:rapperName', (req, res) => {
+    const rappersName = req.params.rapperName.toLowerCase()
+    if (rappers[rappersName]) {
+        res.json(rappers[rappersName])
+    } else {
+        res.json(rappers["Dylan"])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is running on ${PORT} :P`)
+})
